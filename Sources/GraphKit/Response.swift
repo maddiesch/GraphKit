9a3e848be87c0fc1da2c149baa4556bf08e31a37
Maddie Schipper <me@maddiesch.com>
2021-04-08 19:09:59 -0500

    Add more generic query publisher helper

--- Sources/GraphKit/Response.swift
+++ Sources/GraphKit/Response.swift
@@ -7,13 +7,21 @@
 
 import Foundation
 
-public struct Response<Resource : Decodable> : Decodable {
+public struct Response<Resource : Decodable, Error : Decodable> : Decodable {
     public let data: Resource?
-    public let errors: [GraphError]?
+    public let errors: [Error]?
 }
 
 public struct GraphError : Decodable {
+    public struct Location : Decodable {
+        let line: Int?
+        let column: Int?
+        let sourceName: String?
+    }
     
+    let path: String?
+    let message: String?
+    let locations: [Location]?
 }
 
 public enum ResponseError : Error {
--- Sources/GraphKit/URLSession.swift
+++ Sources/GraphKit/URLSession.swift
@@ -9,7 +9,7 @@ import Foundation
 import Combine
 
 extension URLSession {
-    public func execute<QueryType : Query>(query: QueryType, at url: URL, decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<QueryType.ResultType, Error> {
+    public func execute<QueryType : Query, ErrorType : Decodable>(query: QueryType, errorType: ErrorType.Type, at url: URL, decoder: JSONDecoder) -> AnyPublisher<Response<QueryType.ResultType, ErrorType>, Error> {
         do {
             let request = try query.createRequest(for: url)
             
@@ -17,22 +17,25 @@ extension URLSession {
                 .require(httpStatusWithinRange: (200..<201))
                 .retry(3)
                 .map(\.data)
-                .decode(type: Response<QueryType.ResultType>.self, decoder: decoder)
-                .tryMap { response -> QueryType.ResultType in
-                    if let error = response.errors {
-                        throw ResponseError.errors(error)
-                    }
-                    
-                    guard let responseData = response.data else {
-                        throw ResponseError.missingData
-                    }
-                    
-                    return responseData
-                }
+                .decode(type: Response<QueryType.ResultType, ErrorType>.self, decoder: decoder)
                 .eraseToAnyPublisher()
         } catch {
             return Fail(error: error).eraseToAnyPublisher()
         }
+    }
+    
+    public func execute<QueryType : Query>(query: QueryType, at url: URL, decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<QueryType.ResultType, Error> {
+        return self.execute(query: query, errorType: GraphError.self, at: url, decoder: decoder).tryMap { response -> QueryType.ResultType in
+            if let error = response.errors {
+                throw ResponseError.errors(error)
+            }
+            
+            guard let responseData = response.data else {
+                throw ResponseError.missingData
+            }
+            
+            return responseData
+        }.eraseToAnyPublisher()
     }
 }
 
